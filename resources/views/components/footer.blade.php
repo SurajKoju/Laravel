1f15df95d6bef09a3f4654a4745c001324dabe74
Suraj Koju <51220236+SurajKoju@users.noreply.github.com>
2024-06-14 08:44:22 +0000

    Components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+
+
+    <div class = "footer_section">
+        <h1 style="text-align: center;">Title Footer</h1>
+    </div>
+        
+
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<x-header class="contact_header">
+    Contact
+    <x-slot name="message">
+        Important Message
+    </x-slot>
+
+</x-header>
+
+<x-footer></x-footer>
